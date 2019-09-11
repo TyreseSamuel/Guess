a6ebe73c714512b29ee8e7eb46b11328c14524bf
Tyrese Samuel <3tyrese@gmail.com>
2019-09-11 17:53:47 -0500

    Add project files.

---
+++ Guess/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+
+       
+
+        static void Main(string[] args)
+        {
+            string choice = "";
+
+            //Ask for the number
+            Console.WriteLine("Ight fam I'm thinking of a number between 1 and 20.");
+            Console.ReadLine();
+
+            Random rand = new Random();
+            rand.Next(1, 20);
+
+            for (int i = 1; i < 20 && i > 20; i++)
+            {
+                if (i is < rand)
+                {
+                    Console.WriteLine("Sike. Higher.");
+                }
+                if (i is > rand)
+                {
+                    Console.WriteLine("Sike. Lower.");
+                }
+                if (i is == rand)
+                {
+                    Console.WriteLine("Sike. Lemme stop. You got it right.");
+                }
+            }
+        }
+    }
+}
